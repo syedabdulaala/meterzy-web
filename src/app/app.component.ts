a5ca279d26ce3b1b39f96fb534449428524e52d1
abdul.aala <abdul.aala@tenpearls.com>
2018-10-26 16:00:44 +0500

    Added base layouts and import all possible mat modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-meterzy',
+  template: `<router-outlet></router-outlet>`,
+  styles: []
 })
-export class AppComponent {
-  title = 'MeterzyWeb';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,67 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/layout/main-layout/main-layout.component';
+import { SimpleLayoutComponent } from './shared/layout/simple-layout/simple-layout.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent
+  },
+  {
+    path: '',
+    component: SimpleLayoutComponent
+  }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatListModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    SimpleLayoutComponent,
+    MainLayoutComponent
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
