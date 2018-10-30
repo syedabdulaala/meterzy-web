44483b0e6f71c1c0f8b8d9461a8534bf1c9abe0a
abdul.aala <abdul.aala@tenpearls.com>
2018-10-30 11:54:01 +0500

    Added mock functional reading UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { Routes } from '@angular/router';
 
+import { MatNativeDateModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -19,16 +20,24 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/layout/main-layout/main-layout.component';
 import { SimpleLayoutComponent } from './shared/layout/simple-layout/simple-layout.component';
 import { ConfirmDialogComponent } from './shared/dialogs/confirm-dialog/confirm-dialog.component';
+import { ReadingsComponent } from './pages/readings/readings.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MainLayoutComponent
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'readings',
+        component: ReadingsComponent
+      }
+    ]
   },
   {
     path: '',
@@ -43,6 +52,7 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    MatNativeDateModule,
     MatCardModule,
     MatInputModule,
     MatFormFieldModule,
@@ -55,13 +65,15 @@ const routes: Routes = [
     MatDividerModule,
     MatSelectModule,
     MatDialogModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatDatepickerModule
   ],
   declarations: [
     AppComponent,
     SimpleLayoutComponent,
     MainLayoutComponent,
     ConfirmDialogComponent,
+    ReadingsComponent
   ],
   entryComponents: [ConfirmDialogComponent],
   bootstrap: [AppComponent]
